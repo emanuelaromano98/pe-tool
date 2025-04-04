630664cefa4499a0e6dca64ed69ee7a0f4170bab
emanuelaromano98 <emauelaromano1998@gmail.com>
2025-04-03 22:20:55 -0400

    update

--- git.sh
+++ git.sh
@@ -1,16 +1,3 @@
-# git add .
-# read -p "Enter commit message: " message
-
-# if [ -z "$message" ]; then
-#     message="update"
-# fi
-
-# git commit -m "$message"
-# git push origin main
-
-# echo "Changes committed and pushed to origin/main"
-
-
 git add .
 git commit -m "update"
 git push origin main
